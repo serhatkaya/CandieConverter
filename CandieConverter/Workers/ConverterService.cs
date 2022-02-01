016ca50e21d16420176a34da1573d29de756b441
Serhat KAYA <serhat.kaya@emakina.com.tr>
2022-02-01 12:23:56 +0300

    libman conf

--- CandieConverter/Workers/ConverterService.cs
+++ CandieConverter/Workers/ConverterService.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -28,57 +30,71 @@ namespace CandieConverter.Workers
 
 		protected override async Task ExecuteAsync(CancellationToken cancellationToken)
 		{
+			List<Task> tasks = new List<Task>();
+
 			await foreach (var request in _channel.ReadAllAsync(cancellationToken))
 			{
-				try
+				tasks.Add(Task.Run(() => Convert(request)));
+			}
+
+			while (!cancellationToken.IsCancellationRequested)
+			{
+				var toProcess = tasks.ToList();
+				await Task.WhenAll(toProcess).ContinueWith((x) => tasks.Except(toProcess));
+			}
+		}
+
+		private void Convert(ConvertModel request)
+		{
+			try
+			{
+				System.Console.WriteLine($"_converting: {request.FileName} - {DateTime.Now.ToString("HH:mm:ss")}");
+				if (File.Exists(request.Path))
 				{
-					if (File.Exists(request.Path))
+					using (IMagickImage image = new MagickImage(request.Path))
 					{
-						using (IMagickImage image = new MagickImage(request.Path))
+						string fileFolder = Path.GetDirectoryName(request.Path);
+						string fileName = request.FileName.Split(".")[0];
+						//default jpeg.
+						string format = "JPEG";
+						// Save frame as jpg
+						switch (request.TargetFormat)
 						{
-							string fileFolder = Path.GetDirectoryName(request.Path);
-							string fileName = request.FileName.Split(".")[0];
-							//default jpeg.
-							string format = "JPEG";
-							// Save frame as jpg
-							switch (request.TargetFormat)
-							{
-								case "JPG":
-									format = "JPG";
-									image.Format = MagickFormat.Jpg;
-									break;
-								case "JPEG":
-									format = "JPEG";
-									image.Format = MagickFormat.Jpeg;
-									break;
-								case "PNG":
-									format = "PNG";
-									image.Format = MagickFormat.Png;
-									break;
-								default:
-									format = "JPEG";
-									image.Format = MagickFormat.Jpeg;
-									break;
-							}
-
-							string fullOutputPath = Path.Combine(fileFolder, "candieOutput", $"{fileName}.{format}");
-							string outputFolder = fullOutputPath.Replace(Path.GetFileName(fullOutputPath), string.Empty);
-
-							if (!Directory.Exists(outputFolder))
-							{
-								Directory.CreateDirectory(outputFolder);
-							}
-
-							await image.WriteAsync(fullOutputPath);
-							await _hub.SendFileConvertedMessageAsync(request.FileName);
+							case "JPG":
+								format = "JPG";
+								image.Format = MagickFormat.Jpg;
+								break;
+							case "JPEG":
+								format = "JPEG";
+								image.Format = MagickFormat.Jpeg;
+								break;
+							case "PNG":
+								format = "PNG";
+								image.Format = MagickFormat.Png;
+								break;
+							default:
+								format = "JPEG";
+								image.Format = MagickFormat.Jpeg;
+								break;
 						}
+
+						string fullOutputPath = Path.Combine(fileFolder, "candieOutput", $"{fileName}.{format}");
+						string outputFolder = fullOutputPath.Replace(Path.GetFileName(fullOutputPath), string.Empty);
+
+						if (!Directory.Exists(outputFolder))
+						{
+							Directory.CreateDirectory(outputFolder);
+						}
+
+						image.Write(fullOutputPath);
+						_hub.SendFileConvertedMessageAsync(request.FileName).Wait();
 					}
 				}
-				catch (Exception e)
-				{
-					await _hub.SendFileConvertFailedMessageAsync(request.FileName);
-					_logger.LogError(e, "An unhandled exception occured");
-				}
+			}
+			catch (Exception e)
+			{
+				_hub.SendFileConvertFailedMessageAsync(request.FileName).Wait();
+				_logger.LogError(e, "An unhandled exception occured");
 			}
 		}
 	}
